a39f13334e64d57b4ecff87af6fba67036232e51
dhruv patel <dhruvjpatel5@gmail.com>
2025-11-10 22:07:47 +0530

    ada card css

--- client/src/components/Product.jsx
+++ client/src/components/Product.jsx
@@ -2,28 +2,33 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Rating from "react-rating";
 import "font-awesome/css/font-awesome.min.css";
+import "./Product.css";
 
 export default function Product({ product }) {
   console.log("Product data:", product);
   
   return (
     <div className="product-card">
-      <Link to={`/product/${product._id}`} style={{ textDecoration: "none" }}>
-        <img 
-          src={product.image} 
-          className="product-image text-center" 
-          alt={product.name} 
-        ></img>
-        <h1 className="product-title">{product.name}</h1>
-        <div className="rating-stars">
-          <Rating
-            initialRating={product.rating}
-            emptySymbol="fa fa-star-o fa-1x"
-            fullSymbol="fa fa-star fa-1x"
-            readonly={true}
+      <Link to={`/product/${product._id}`} style={{ textDecoration: "none", color: "inherit" }}>
+        <div className="image-container">
+          <img 
+            src={product.image} 
+            className="product-image" 
+            alt={product.name} 
           />
         </div>
-        <h1 className="product-price">Price: ${product.price}</h1>
+        <div className="product-content">
+          <h2 className="product-title">{product.name}</h2>
+          <div className="rating-stars">
+            <Rating
+              initialRating={product.rating}
+              emptySymbol="fa fa-star-o fa-1x"
+              fullSymbol="fa fa-star fa-1x"
+              readonly={true}
+            />
+          </div>
+          <p className="product-price">${product.price}</p>
+        </div>
       </Link>
     </div>
   );
